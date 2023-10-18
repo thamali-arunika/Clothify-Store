f4a8d14f02a45acb23d3d84a752521308e53571c
thamali-arunika <thamaliaruni@gmail.com>
2023-10-18 08:25:58 +0530

    create crud util class

--- src/main/java/crudUtil/CrudUtil.java
+++ src/main/java/crudUtil/CrudUtil.java
@@ -1,5 +1,22 @@
 package crudUtil;
 
+import db.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CrudUtil {
+    public static <S> S execute(String sql, Object... args) throws SQLException, ClassNotFoundException {
+        PreparedStatement psTm = DBConnection.getInstance().getConnection()
+                .prepareStatement(sql);
+
+        for (int i = 0; i < args.length; i++) {
+            psTm.setObject((i + 1), args[i]);
+        }
+        if (sql.startsWith("SELECT") || sql.startsWith("select")) {
+            return (S) psTm.executeQuery();
+        }
+        return (S) (Boolean)(psTm.executeUpdate()>0);
+}
 
 }
